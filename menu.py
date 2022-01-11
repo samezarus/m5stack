14b8c1e51c46dc5afe8ce5f11c2ab10741e7f8c2
sameza <samezarus@gmail.com>
2022-01-11 23:18:47 +1000

    Update menu.py

--- menu.py
+++ menu.py
@@ -15,15 +15,21 @@ setScreenColor(bg_color)
 
 class Header:
   def __init__(self):
-    self.color = 0x8d8282
-    self.rectangle = M5Rect(0, 0, 320, 32, self.color, self.color)
-    self.label = M5TextBox(x=5, y=5, text="Header", font=lcd.FONT_DejaVu24, color=0xFFFFFF, rotate=0)
+    self.bg_color = 0x8d8282
+    self.text_color = 0xFFFFFF
+    self.rectangle = M5Rect(0, 0, 320, 32, self.bg_color, self.bg_color)
+    self.label = M5TextBox(x=5, y=5, text="Header", font=lcd.FONT_DejaVu24, color=self.text_color, rotate=0)
     
   def setText(self, text: str):
-    self.rectangle.setBgColor(self.color)
-    self.label.setText(text)
+    self.rectangle = M5Rect(0, 0, 320, 32, self.bg_color, self.bg_color)
+    self.label = M5TextBox(x=5, y=5, text=text, font=lcd.FONT_DejaVu24, color=self.text_color, rotate=0)
+    #self.rectangle.setBgColor(self.color)
+    #self.label.setText(text)
     
 
 header = Header()
 header.setText("new")
 header.setText("11")
+#header.setText("0")
+
+
