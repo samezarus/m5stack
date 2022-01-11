6d7ee52ae930c45114cf41d7ce53a10d651c07cc
sameza <samezarus@gmail.com>
2022-01-11 23:29:48 +1000

    Update menu.py

--- menu.py
+++ menu.py
@@ -17,19 +17,24 @@ class Header:
   def __init__(self):
     self.bg_color = 0x8d8282
     self.text_color = 0xFFFFFF
-    self.rectangle = M5Rect(0, 0, 320, 32, self.bg_color, self.bg_color)
-    self.label = M5TextBox(x=5, y=5, text="Header", font=lcd.FONT_DejaVu24, color=self.text_color, rotate=0)
+    
+    self.r_x = 0
+    self.r_y = 0
+    self.r_w = 320
+    self.r_h = 32
+    self.rectangle = M5Rect(self.r_x, self.r_y, self.r_w, self.r_h, self.bg_color, self.bg_color)
+    
+    self.l_x = 5
+    self.l_y = 5
+    self.label = M5TextBox(self.l_x, self.l_y, text="Header", font=lcd.FONT_DejaVu24, color=self.text_color, rotate=0)
     
   def setText(self, text: str):
-    self.rectangle = M5Rect(0, 0, 320, 32, self.bg_color, self.bg_color)
-    self.label = M5TextBox(x=5, y=5, text=text, font=lcd.FONT_DejaVu24, color=self.text_color, rotate=0)
+    self.rectangle = M5Rect(self.r_x, self.r_y, self.r_w, self.r_h, self.bg_color, self.bg_color)
+    self.label = M5TextBox(self.l_x, self.l_y, text=text, font=lcd.FONT_DejaVu24, color=self.text_color, rotate=0)
     #self.rectangle.setBgColor(self.color)
     #self.label.setText(text)
     
 
 header = Header()
-header.setText("new")
-header.setText("11")
+header.setText("/")
 #header.setText("0")
-
-
